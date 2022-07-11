e0c850d752ca78a83911a19c1afa53ea4ca66316
Gaël Reyrol <me@gaelreyrol.dev>
2022-07-11 12:01:34 +0200

    feat: add alacritty settings

--- home.nix
+++ home.nix
@@ -79,6 +79,47 @@
   #   };
   # };
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal.family = "JetBrains Mono";
+        size = 12.0;
+      };
+
+      colors = {
+        primary = {
+          background = "#fdf6e3";
+          foreground = "#657b83";
+        };
+        cursor = {
+          text = "#fdf6e3";
+          curser = "#657b83";
+        };
+        normal = {
+          black = "#073642";
+          red = "#dc322f";
+          green = "#859900";
+          yellow = "#b58900";
+          blue = "#268bd2";
+          magenta = "#d33682";
+          cyan = "#2aa198";
+          white = "#eee8d5";
+        };
+        bright = {
+          black = "#002b36";
+          red = "#cb4b16";
+          green = "#586e75";
+          yellow = "#657b83";
+          blue = "#839496";
+          magenta = "#6c71c4";
+          cyan = "#93a1a1";
+          white = "#fdf6e3";
+        };
+      };
+    }
+  };
+
   programs.git = {
     enable = true;
     userName = "Gaël Reyrol";
