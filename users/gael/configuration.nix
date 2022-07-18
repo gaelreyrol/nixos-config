0da3e23605265fd88e60d08e6574b1e3b4ceb14e
Gaël Reyrol <me@gaelreyrol.dev>
2022-07-18 11:16:49 +0200

    fix: disable pam setup

--- users/gael/configuration.nix
+++ users/gael/configuration.nix
@@ -14,24 +14,24 @@
     polkitPolicyOwners = [ "gael" ];
   };
 
-  security.pam.yubico = {
-    enable = true;
-    debug = true;
-    control = "required";
-    mode = "challenge-response";
-  };
+  # security.pam.yubico = {
+  #   enable = true;
+  #   debug = true;
+  #   control = "required";
+  #   mode = "challenge-response";
+  # };
 
-  systemd.services."xlock" = {
-    description = "Lock all sessions with loginctl";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.systemd}/bin/loginctl lock-sessions";
-    };
-  };
+  # systemd.services."xlock" = {
+  #   description = "Lock all sessions with loginctl";
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     ExecStart = "${pkgs.systemd}/bin/loginctl lock-sessions";
+  #   };
+  # };
 
   ## Start xlock.service on unplugged Yubikey 
-  services.udev.extraRules = ''
-    ACTION=="remove", SUBSYSTEM=="input", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0110|0111|0114|0116|0401|0403|0405|0407|0410", ENV{ID_SECURITY_TOKEN}="1", RUN+="${pkgs.systemd}/bin/systemctl start xlock.service"
-  '';
+  # services.udev.extraRules = ''
+  #   ACTION=="remove", SUBSYSTEM=="input", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0110|0111|0114|0116|0401|0403|0405|0407|0410", ENV{ID_SECURITY_TOKEN}="1", RUN+="${pkgs.systemd}/bin/systemctl start xlock.service"
+  # '';
 }
 
