5721371f4c110ec3d8105e0c3835ab91e02d1984
Gaël Reyrol <me@gaelreyrol.dev>
2022-07-28 09:59:56 +0200

    feat(users/gael): add new dconf

--- users/gael/dconf.nix
+++ users/gael/dconf.nix
@@ -5,8 +5,31 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+      color-scheme = "default";
+      locate-pointer = true;
+      clock-show-date = true;
+    };
+
     "org/gnome/desktop/calendar" = {
       show-weekdate = true;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      focus-mode = "sloppy";
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      clock-format = "24h";
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
     };
 
     "org/gnome/shell" = {
@@ -23,6 +46,5 @@ with lib.hm.gvariant;
     "org/gnome/shell/extensions/caffeine" = {
       user-enabled = true;
     };
-
   };
 }
