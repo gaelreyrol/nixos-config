18b507709cd36c12fca7933252f53e43fa7b41a9
Gaël Reyrol <me@gaelreyrol.dev>
2022-11-28 11:43:03 +0100

    feat(hosts/tower): force strongswan disabled service

--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -2,13 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   networking.hostName = "tower"; # Define your hostname.
   networking.networkmanager.enable = true;
 
-  systemd.services.strongswan.wantedBy = [ ];
+  systemd.services.strongswan.wantedBy = lib.mkForce [ ];
 
   time.timeZone = "Europe/Paris";
 
