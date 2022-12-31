ecdaacb59d05065db4048502e874935295de7677
Gaël Reyrol <me@gaelreyrol.dev>
2022-12-31 12:21:07 +0100

    feat(hosts/tower): change home state version to 22.11

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
   networking.hostName = "thinkpad";
   networking.networkmanager.enable = true;
 
@@ -132,13 +140,5 @@
   };
 
   environment.shells = with pkgs; [ fish ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
 }
 
--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -1,6 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?}
+
   networking.hostName = "tower"; # Define your hostname.
   networking.networkmanager.enable = true;
 
@@ -135,13 +143,5 @@
   };
 
   environment.shells = with pkgs; [ fish ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?}
 }
 
