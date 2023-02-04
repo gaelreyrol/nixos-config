fd01205b0d92ed71d3343432e2031b434b79aef1
Gaël Reyrol <me@gaelreyrol.dev>
2023-02-04 17:24:37 +0100

    fix: update renamed options

--- hosts/pi0/configuration.nix
+++ hosts/pi0/configuration.nix
@@ -16,8 +16,10 @@
 
   services.openssh = {
     enable = true;
-    passwordAuthentication = false;
-    permitRootLogin = "no";
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
   };
 
   time.timeZone = "Europe/Paris";
