5b23a47c4428ea679481e1c5e5595b531f1707f0
Gaël Reyrol <me@gaelreyrol.dev>
2023-06-18 16:18:14 +0200

    chore(mixins/power): add docs

--- mixins/power.nix
+++ mixins/power.nix
@@ -2,6 +2,8 @@
 
 {
   services.udev.extraRules = pkgs.udev-nix.toUdevFile "power-supply.rules" {
+    # Test with: sudo udevadm test (udevadm info /sys/class/power_supply/AC --query=path)
+    # Documentation: https://docs.kernel.org/power/power_supply_class.html
     rules = with pkgs.udev-nix; {
       "Power Saver mode" = {
         Subsystem = operators.match "power_supply";
