48729992165b0a55adc3d0650a621de0dd49e68f
Gaël Reyrol <me@gaelreyrol.dev>
2023-08-13 12:34:39 +0200

    fix(tailscale): wrong tailscale service 'wants' target

--- mixins/tailscale.nix
+++ mixins/tailscale.nix
@@ -18,8 +18,8 @@
     description = "Automatic connection to Tailscale";
 
     # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
+    after = [ "network-pre.target" "tailscaled.service" ];
+    wants = [ "network-pre.target" "tailscaled.service" ];
     wantedBy = [ "multi-user.target" ];
 
     # set this service as a oneshot job
