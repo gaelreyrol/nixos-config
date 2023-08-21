5e917e64cb4853a7d46e44608844c90edb2c014e
Gaël Reyrol <me@gaelreyrol.dev>
2023-08-21 13:07:18 +0200

    fix(hosts/pi0): use unstable home-assistant

--- hosts/pi0/configuration.nix
+++ hosts/pi0/configuration.nix
@@ -98,7 +98,7 @@
     extraComponents = [
       "air_quality"
       "co2signal"
-      "discovery"
+      # "discovery"
       "esphome"
       "http"
       "meteo_france"
@@ -117,39 +117,7 @@
       pyqrcode
       pyotp
     ];
-    package = pkgs.home-assistant.override {
-      # https://github.com/NixOS/nixpkgs/pull/234880
-      packageOverrides = self: super: {
-        aiohttp = super.aiohttp.overrideAttrs (oldAttrs: {
-          patches = (oldAttrs.patches or [ ]) ++ [
-            (pkgs.fetchpatch {
-              url = "https://github.com/aio-libs/aiohttp/commit/7dcc235cafe0c4521bbbf92f76aecc82fee33e8b.patch";
-              hash = "sha256-ZzhlE50bmA+e2XX2RH1FuWQHZIAa6Dk/hZjxPoX5t4g=";
-            })
-          ];
-        });
-        uvloop = super.uvloop.overridePythonAttrs (oldAttrs: {
-          disabledTestPaths = (oldAttrs.disabledTestPaths or [ ]) ++ [
-            "tests/test_regr1.py"
-          ];
-        });
-        yarl = super.yarl.overrideAttrs (oldAttrs: rec {
-          version = "1.9.2";
-          src = pkgs.fetchPypi {
-            inherit (oldAttrs) pname;
-            inherit version;
-            hash = "sha256-BKudS59YfAbYAcKr/pMXt3zfmWxlqQ1ehOzEUBCCNXE=";
-          };
-          disabledTests = [ ];
-        });
-        snitun = super.snitun.overridePythonAttrs (oldAtts: {
-          doCheck = false;
-        });
-        zigpy-znp = super.zigpy-znp.overridePythonAttrs (oldAtts: {
-          doCheck = false;
-        });
-      };
-    };
+    package = pkgs.unstable.home-assistant;
   };
 
   networking.firewall.enable = lib.mkDefault false;
